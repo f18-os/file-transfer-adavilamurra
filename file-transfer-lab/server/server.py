06c4132c3bef8c85ba140ceec9bc20d40c93a70b
Alejandro Davila <adavilamurra@miners.utep.edu>
2018-10-07 20:34:48 -0600

    Added sercer and Readme

--- file-transfer-lab/server/server.py
+++ file-transfer-lab/server/server.py
@@ -1,24 +1,28 @@
 from socket import *
-import sys
+import sys, os
 
 class Server:
     def __init__(self):
         self.listenerSocket = self.socketListen()  #create listen socket
         self.connSocket, self.addr = None, ""
-        self.file_name, self.choice, self.strBuffer = "", "", ""
-        self.serverAddr = ("", 0)
+        self.file_name, self.choice, self.restOfBuffer = "", "", ""
+        self.numClients, self.serverAddr = 0, ("", 0)
         self.acceptConnection()     #accept connection from client
-    
+        self.getNumberOfClients()   #get the number of clients
+        self.connectToClients()
+        
     #put method from client   
     def getFileFromClient(self):
-        if self.strBuffer == "File not found. Try again.":
+        if self.restOfBuffer == "File not found. Try again.":
             return
+        if os.path.exists(self.file_name):
+            print("This file already exists. Overwriting...")
         with open(self.file_name, "w") as serverFile:
-            serverFile.write(self.strBuffer)
+            serverFile.write(self.restOfBuffer)
             while True:
-                #print("Receiving data...")
+                print("Receiving data...")
                 data = self.connSocket.recv(1024).decode()
-                if self.strBuffer == "File not found. Try again." or data == "File not found. Try again.":
+                if self.restOfBuffer == "File not found. Try again." or data == "File not found. Try again.":
                     return
                 if not data:
                     break
@@ -27,7 +31,6 @@ class Server:
                 serverFile.write(data)    
         serverFile.close()
         print("Successfully got file from client.")
-        return
 
     #get method from client
     def sendFileToClient(self):
@@ -43,7 +46,6 @@ class Server:
             self.connSocket.close()
             return
         print("Successfuly sent file to client.")
-        return
 
     def determineChoice(self):
         if(self.choice.lower() == "put"):
@@ -51,40 +53,65 @@ class Server:
         elif(self.choice.lower() == "get"):
             self.sendFileToClient()
         else:
-            print("Invalid choice. Choices: PUT or GET.")
-        return
+            print("Invalid choice.")
 
     def getNameAndChoice(self):
         while True:
-            #print("Getting name and choice...")
             nameChoice = self.connSocket.recv(1024).decode().split(":")
-            #print("Name:choice= ", nameChoice)
-            self.file_name = nameChoice[0]
-            #print("File: ", self.file_name)
+            if self.numClients == 0:
+                self.file_name = nameChoice[0]
+            else:
+                self.file_name = nameChoice[0][1:]
             try:
                 if(len(nameChoice[1]) > 3):
                     self.choice = nameChoice[1][:3]
-                    self.strBuffer = nameChoice[1][3:]
+                    self.restOfBuffer = nameChoice[1][3:]
                 else:
                     self.choice = nameChoice[1]
-                    self.strBuffer = ""
-                if (self.choice.lower()) == "put" or (self.choice.lower()) == "get":
-                    self.connSocket.send("Done".encode())
-                    #print("Choice: ", self.choice)
-                    #print("Buff: ", self.strBuffer)
+                    self.restOfBuffer = ""
+                if self.choice.lower() == "put" or self.choice.lower() == "get":
+                    self.connSocket.send("Done".encode()) 
                 return
             except:
                 print("Error. Data did not arrive correctly.")
                 self.connSocket.send("Error".encode())
+                return
+
+    def forkClient(self, client):
+        rc = os.fork()
+        if rc < 0:       # fork failed
+            print("Fork failed, returning ", rc)
+        elif rc == 0:      # child
+            if client > 0:
+                self.acceptConnection()
+            self.getNameAndChoice()
+            self.determineChoice()
+        else:          # parent
+            os.wait()
+        
+    def connectToClients(self):
+        # check for connections depending on the number of clients
+        if self.numClients < 1:
+            print("You need at least one client.")
+            sys.exit()
+        elif self.numClients == 1:
+            self.getNameAndChoice()
+            self.determineChoice()
+        else:
+            for client in range(self.numClients):
+                self.forkClient(client)
     
+    def getNumberOfClients(self):
+        self.numClients = int(self.connSocket.recv(1).decode())
+
     def acceptConnection(self):
         while True:
             self.connSocket, self.addr = self.listenerSocket.accept()      #establish connection with client.
             print("Got connection from", self.addr)
-            return 
+            return
 
     def socketListen(self):
-        self.serverAddr = ("127.0.0.1", 50000)      #set host and address
+        self.serverAddr = ("127.0.0.1", 50001)      #set host and address
         listenerSocket = socket()              #create a socket object
         listenerSocket.bind(self.serverAddr)        #bind to the port
         listenerSocket.listen(1)               #wait for client connection.
@@ -94,12 +121,10 @@ class Server:
     def closeConnection(self):
         self.listenerSocket.close()
         self.connSocket.close()
-        print("---- Connection closed. ---- \n")
+        print("---- Connection closed. ----")
 
 def startServer():
-    server = Server()    
-    server.getNameAndChoice()
-    server.determineChoice()
+    server = Server()
     server.closeConnection()
 
 startServer()
