74c707da957ee26152dbe3f247a2c1b3bd72905a
Alejandro Davila <adavilamurra@miners.utep.edu>
2018-10-07 20:34:16 -0600

    Added client and Readme

--- file-transfer-lab/client/client.py
+++ file-transfer-lab/client/client.py
@@ -2,9 +2,9 @@ from socket import *
 import sys
 
 class Client:
-    def __init__(self, file_name, choice):
-        self.sAddr = ("127.0.0.1", 50000)                #set address and reserve port
-        self.file_name, self.choice = file_name, choice
+    def __init__(self, file_name, choice, numClients):
+        self.sAddr = ("127.0.0.1", 50001)                #set address and reserve port
+        self.numClients, self.file_name, self.choice = numClients, file_name, choice
         self.clientSock = socket(AF_INET, SOCK_STREAM)      #create client socket
         try:
             self.clientSock.connect(self.sAddr)          #connect to Server
@@ -12,6 +12,7 @@ class Client:
         except error:
             print("Error. Server not found.")
             sys.exit()
+        self.sendNumClients()               #send number of clients to server
         self.sendBasicInfoToServer()        #send file name and choice to server
         self.determineChoice()              #determine if put or get
         self.closeConnection()              #close socket connection
@@ -20,14 +21,14 @@ class Client:
     def getFileFromServer(self):
         with open(self.file_name, "w") as clientFile:
             while True:
-                #print("Receiving data...")
+                print("Receiving data...")
                 data = self.clientSock.recv(1024).decode()
                 if data == "File not found. Try again.":
                     print(data)
                     return
                 if not data:
                     break
-                print("Data Received = ", data)
+                print("Data received =", data)
                 # write data to a file
                 clientFile.write(data)    
         clientFile.close()
@@ -48,32 +49,36 @@ class Client:
         print("Successfuly sent file to server.")
 
     def determineChoice(self):
-        if(self.choice.lower() == "put"):
+        if(self.choice == "PUT" or self.choice == "put"):
             self.sendFileToServer()
-        elif(self.choice.lower() == "get"):
+        elif(self.choice == "GET" or self.choice == "get"):
             self.getFileFromServer()
         else:
             print("Invalid choice. Choices: PUT or GET.")
     
     def sendBasicInfoToServer(self):
         while True:
-            basicInfo = self.file_name + ":" + self.choice
             #send file name and choice and if the ack is not received, send again
-            print(basicInfo)
-            #print("Sending basic info to server...")
-            self.clientSock.send(basicInfo.encode())     #send file name and choice to server
+            self.clientSock.send((self.file_name + ":" + self.choice).encode())     #send file name and choice to server
             ack = self.clientSock.recv(4).decode()
-            #print("ACK= ", ack)
             if ack == "Done":
                 return
+
+    def sendNumClients(self):
+        self.clientSock.send(str(self.numClients).encode())     #send number of Clients to server
     
     def closeConnection(self):
         self.clientSock.close()
         print("--- Connection closed. --- \n")
 
 def startClient():
-    file_name = input('Enter file name: ')
-    choice = input('Enter choice (PUT or GET): ')
-    client = Client(file_name, choice)
+    numClients = input('Enter the number of clients: ')
+    clients = []
+    #iterate with the number of clients
+    for i in range(int(numClients)):
+        print("Client ", str(i+1))
+        file_name = input('Enter file name: ')
+        choice = input('Enter choice (PUT or GET): ')
+        clients.append(Client(file_name, choice, numClients))
         
 startClient()
